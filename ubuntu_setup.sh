a373da39193080f99e940997ef5cd0bc7f15d284
Patrick Bassing <patbassing@users.noreply.github.com>
2019-01-30 09:20:38 +0100

    Updated ubuntu_setup.sh. VS Code is now a snap

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -38,13 +38,7 @@ sudo apt-get update
 sudo apt-get install -y dotnet-sdk-2.2
 
 # VS Code
-cd ~/Downloads
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get install -y apt-transport-https
-sudo apt-get update
-sudo apt-get install -y code # or code-insiders
+sudo snap install vscode --classic
 
 # Docker
 sudo apt-get remove docker docker-engine docker.io containerd runc
